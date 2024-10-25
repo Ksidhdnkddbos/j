a174a359ade481b6c12fcac7d3d75e031a7da5f1
FINAL AINZ <133069804+1mrxe1@users.noreply.github.com>
2024-10-25 17:13:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 from sample_config import Config
 class Development(Config):
     # ايبي ايدي 
-    APP_ID = API_ID
-    API_HASH = "API_HASH"
+    APP_ID = 23240929
+    API_HASH = "c86e205a2bca8d6381b30a0d7681bba0"
     # ايبي هاش
     ALIVE_NAME = "Your_name"
     # لاتغيرها
     DB_URI = "postgresql://postgres:final111@localhost:5432/finalv"
     #كود التيرمكس
-    STRING_SESSION = "your_sessions"
+    STRING_SESSION = "1ApWapzMBu5j-peRHh50adVOqAIcSzZx_8o6H3Q-Y-OSWrMbd46eJOH-sDdM5Wvqn8sCYt3H05zLJIgLrON2m6YuJvMRXv9dd8DEQXRtmbNCNz00omVoeuRxfmZ3vvnw8bipkWuvumMvgpNWol8y1WSc3QWjBiPyNTy4V5sCsFT6vG9YxaI386OX9B3Vs1aI_QJAvqOrqgcqyAWyYTtG46NUFCLGtAQrdJ5T3rmTsCMyQ_4g23qGuddYpQ7dy451FxInNsctCwh3gnum42cjzU7fWqOa4tvs3EjClOiVJU8q72-ZLCqCiBM1Ou65_d6LQp1ajnpG4Kklso9u9wRWiT_e8Y6L4x3o="
     # توكن البوت
-    TG_BOT_TOKEN = "TOKEN_BOT"
+    TG_BOT_TOKEN = "7842554655:AAE6luS4c4bKlA-L3OF9KHDKmYIEKH2Ll4E"
     # لاتغيرها
     COMMAND_HAND_LER = "."
     # لاتغيرها
